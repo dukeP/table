07fb19f0d6edb1aeef0a10680a53dcf365ad0742
dukeP <wp920920@sina.com>
2020-09-07 11:50:20 +0800

    Update stickyScrollBar.tsx
    fix sticky

--- src/stickyScrollBar.tsx
+++ src/stickyScrollBar.tsx
@@ -11,6 +11,37 @@ interface StickyScrollBarProps {
   onScroll: (params: { scrollLeft?: number }) => void;
   offsetScroll: number;
 }
+const getScrollParent = (node) => {
+  const regex = /(auto|scroll)/;
+  const parents = (_node, ps) => {
+    if (_node.parentNode === null) { return ps; }
+    return parents(_node.parentNode, ps.concat([_node]));
+  };
+
+  const style = (_node, prop) => getComputedStyle(_node, null).getPropertyValue(prop);
+  const overflow = _node => style(_node, 'overflow') + style(_node, 'overflow-y') + style(_node, 'overflow-x');
+  const scroll = _node => regex.test(overflow(_node));
+
+  /* eslint-disable consistent-return */
+  const scrollParent = (_node) => {
+    if (!(_node instanceof HTMLElement || _node instanceof SVGElement)) {
+      return;
+    }
+
+    const ps = parents(_node.parentNode, []);
+
+    for (let i = 0; i < ps.length; i += 1) {
+      if (scroll(ps[i])) {
+        return ps[i];
+      }
+    }
+
+    return document.scrollingElement || document.documentElement;
+  };
+
+  return scrollParent(node);
+  /* eslint-enable consistent-return */
+};
 
 const StickyScrollBar: React.ForwardRefRenderFunction<unknown, StickyScrollBarProps> = (
   { scrollBodyRef, onScroll, offsetScroll },
@@ -123,7 +154,7 @@ const StickyScrollBar: React.ForwardRefRenderFunction<unknown, StickyScrollBarPr
   }, [scrollBarWidth, isActive]);
 
   React.useEffect(() => {
-    const onScrollListener = addEventListener(window, 'scroll', onContainerScroll, false);
+    const onScrollListener = addEventListener(getScrollParent(scrollBodyRef.current), 'scroll', onContainerScroll, false);
     const onResizeListener = addEventListener(window, 'resize', onContainerScroll, false);
 
     return () => {
